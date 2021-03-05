a57bcb89d16b88273564cb4de132219fd1ab7990
Ariel Zambrano <arielo774@hotmail.com>
2021-03-05 17:09:35 +0100

    Redimensionar canvas

--- js/admin-controller.js
+++ js/admin-controller.js
@@ -1,1 +1,25 @@
-Prueba
+function getCanvas() {
+    var canvas = document.getElementById("canvas");
+    //var context = theCanvas.getContext("2d");
+    let alcada = document.getElementById("alcada");
+    let amplada = document.getElementById("amplada");
+    // Default values witdh and height
+    alcada.value = canvas.clientWidth;
+    amplada.value = canvas.clientHeight;
+
+    alcada.addEventListener("input", () => {
+        setAlcada(canvas, alcada.value)
+    })
+    amplada.addEventListener("input", () => {
+        setAmplada(canvas, amplada.value)
+    })
+}
+function setAmplada(canvas, amplada) {
+    canvas.height = amplada;
+}
+function setAlcada(canvas, alcada) {
+    canvas.width = alcada;
+}
+function init() {
+    getCanvas();
+}
