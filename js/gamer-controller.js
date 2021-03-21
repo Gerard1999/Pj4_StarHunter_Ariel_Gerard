3b479db28e9681e6ada473aad8a7484924983fb7
Ariel Zambrano <arielo774@hotmail.com>
2021-03-21 16:37:11 +0100

    Actualització JS client

--- js/gamer-controller.js
+++ js/gamer-controller.js
@@ -1,88 +1,19 @@
 /*************************
  * 		DADES GLOBALS
  *************************/
-//Canvas
-var canvas  = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
 //Mesures canvas
-var amplada = canvas.clientWidth;
-var altura = canvas.clientHeight;;
+var amplada = Game.canvas.clientWidth;
+var altura = Game.canvas.clientHeight;
 
-// Variabe nau
+// SpaceShip
 var spaceShip;
 
 //Posicions nau
 var nauX;
 var nauY;
 
-//Imatge
-var img = new Image();
-var cosNau = 64; //64 x 64 píxels
-
 //ID JUGADOR:
 var id;
-
-// Objecte per enmagatzemar la nostra tecla premuda
-var keysPress = {};
-
-/**
- * Funció que es crida al obrir la pàgina, carrega la imatge de la nau
- * al centre inferior de la pantalla
- */
-
-function crearNau () {
-    spaceShip = new Image();
-    spaceShip.src = "../images/nau64px.png";
-    nauX = canvas.clientWidth / 2 - 32;
-    nauY = canvas.clientHeight - 64;
-    connexio.send(JSON.stringify({ action: "move", X: nauX, Y: nauY, id: id }));
-}
-
-/**
- * Funció per moure la nau amb les tecles i s'envia un missatge al
- * servidor amb les coordenades actuals.
- */
-function moureNau(e) {
-    //Variables de controls
-    var dreta = e.key == "ArrowRight" || e.key == "d";
-    var esquerra = e.key == "ArrowLeft" || e.key == "a";
-    var amunt = e.key == "ArrowUp" || e.key == "w";
-    var avall = e.key == "ArrowDown" || e.key == "s";
-
-    //Si s'apreta alguna tecla de control, evita que la pàigna es mogui
-    if (dreta || esquerra || amunt || avall) { e.preventDefault(); }
-
-    if (dreta) {
-        var moure = setInterval(function() {
-            if (nauX + cosNau > amplada) {
-                ctx.clearRect(nauX, nauY, nauX + cosNau, nauY + cosNau);
-                ctx.drawImage(img, amplada - cosNau, nauY);
-            } else {
-                ctx.clearRect(nauX, nauY, nauX + cosNau, nauY + cosNau);
-                ctx.drawImage(img, nauX + 1, nauY);
-                nauX += 1;
-            }
-        }, 1);
-        //Si es deixa d'apretar, cancel·la el setInterval de moure
-        document.addEventListener("keyup", () => { clearInterval(moure); });
-    }
-    if (esquerra) {
-        var moure = setInterval(function() {
-            if (nauX < 0) {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(img, 0, nauY);
-            } else {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(img, nauX - 1, nauY);
-                nauX -= 1;
-            }
-        }, 1);
-        //Si es deixa d'apretar, cancel·la el setInterval de moure
-        document.addEventListener("keyup", () => { clearInterval(moure); });
-    }
-
-}
 
 /**
  * Funció que ubica totes les naus creades i mogudes fins ara.
@@ -90,14 +21,32 @@ function moureNau(e) {
  * @param coordenades : objecte de coordenades de totes les naus creades
  * Format: {id: idJugador, X: posicioX, Y: posicioY}
  */
-function ubicarNaus(coordenades) {
-    coordenades.forEach(nau => {
-        if (nau.id != id) {
-            var image = new Image();
-            image.src = "../images/nau64pxEnemic.png";
-            image.onload = () => { ctx.drawImage(image, nau.X, nau.Y); }
-        }
-    });
+function createNau (nau) {
+    spaceShip = nau; // Asignem la nostra variable global Spaceship i fiquen dintre l'objecte nau
+    id = spaceShip.id; // Afegim l'id del nostre jugador actualss
+    nauX = amplada / 2 - 32;
+    nauY = altura - 64;
+    spaceShip.img = new Image();
+    spaceShip.img.src = "../images/nau64px.png";
+    spaceShip.x = nauX;
+    spaceShip.y = nauY;
+}
+
+function centerNau () {
+    Game.ctx.fillStyle = '#b6ddf6'; // Background del Canvas
+    Game.ctx.fillRect(0, 0, amplada, altura);
+    spaceShip.img.onload = () => {Game.ctx.drawImage(spaceShip.img, nauX, nauY)}; // Centren l'imatge
+}
+
+/**
+ * Funció que es crida al obrir la pàgina, carrega la imatge de la nau
+ * al centre inferior de la pantalla
+ */
+
+function moureNau() {
+	if (connexio) { 
+        connexio.send(JSON.stringify( { action: "move", nau: spaceShip })); 
+    }
 }
 
 /**
@@ -105,61 +54,97 @@ function ubicarNaus(coordenades) {
  */
 
 window.addEventListener("keydown", function (e) {
-  keysPress[e.key] = true; // El true només és per indicar el valor del key del objecte
+  Game.keysPress[e.key] = true; // El true només és per indicar el valor del key del objecte
+  moureNau();
+  e.preventDefault();
+  
 }, false);
 
 window.addEventListener("keyup", function (e) {
   // Amb el event KeyUp el que fem es aturar perquè eliminem la key que es troba en el nostre objecte keysDown
-  // la nau ja que aturen la funció recursiva 
-  delete keysPress[e.key]; 
+  // la nau ja que aturen la funció recursiva
+  delete Game.keysPress[e.key]; 
+  e.preventDefault();
 }, false);
 
-// // //New update with keyboard controls
-function updateCanvas () {
-  // Moure nau amunt
-  if ("ArrowUp" in keysPress || "w" in keysPress) nauY -= 2;
-  // Moure nau abaix
-  if ("ArrowDown" in keysPress || "s" in keysPress) nauY += 2; 
-  // Moure nau ezquerra
-  if ("ArrowLeft" in keysPress || "a" in keysPress) nauX -= 2;
-  // Moure nau dreta
-  if ("ArrowRight" in keysPress || "d" in keysPress) nauX += 2; 
-  ctx.fillStyle = '#b6ddf6' // Background del Canvas
-  ctx.fillRect(0, 0, amplada, altura); // Els primers valors es per on comenza el canvas (X, Y) i els dos següents per l' amplada i açada
-  ctx.drawImage(spaceShip, nauX, nauY); // Dibuixen la nostra nau en una nova posició
-  requestAnimationFrame(updateCanvas) // Actualitzen el canvas amb les noves posicions
+/**
+ * Funció per moure la nau amb les tecles i s'envia un missatge al
+ * servidor amb les coordenades actuals.
+ */
+ function updateCanvas () {
+    spaceShip.img = new Image();
+    spaceShip.img.src = "../images/nau64px.png";
+    // Moure nau amunt
+    if ("ArrowUp" in Game.keysPress || "w" in Game.keysPress) 
+    spaceShip.y > -64 ? spaceShip.y -= spaceShip.speed : spaceShip.y = altura;
+    // Moure nau abaix
+    if ("ArrowDown" in Game.keysPress || "s" in Game.keysPress) 
+        spaceShip.y < altura ? spaceShip.y += spaceShip.speed : spaceShip.y = -64;
+    // Moure nau ezquerra
+    if ("ArrowLeft" in Game.keysPress || "a" in Game.keysPress) 
+        spaceShip.x > -64 ? spaceShip.x -= spaceShip.speed : spaceShip.x = amplada;
+    // Moure nau dreta
+    if ("ArrowRight" in Game.keysPress || "d" in Game.keysPress) 
+        spaceShip.x < amplada ? spaceShip.x += spaceShip.speed : spaceShip.x = -64;
+        
+    Game.ctx.fillStyle = '#b6ddf6' // Background del Canvas
+    Game.ctx.fillRect(0, 0, amplada, altura); // Els primers valors es per on comenza el canvas (X, Y) i els dos següents per l' amplada i açada
+    Game.ctx.drawImage(spaceShip.img, spaceShip.x, spaceShip.y); // Dibuixen la nostra nau en una nova posició
+    Game.render(updateCanvas) // Actualitzen el canvas amb les noves posicions
+    //spaceShip.img.onload = () => {Game.ctx.drawImage(spaceShip.img, spaceShip.x, spaceShip.y)};
+}
+
+
+/* Funció per obrir i tencar una sessió*/
+function openConnection() { 
+	connexio.onopen = function() { // Obrir sessió
+        connexio.send(JSON.stringify({ action: "addPlayer"})
+		);
+	}
+	connexio.onclose = function() { // Si la sessió s'ha tancat
+		alert("Se ha tancat la connexió");
+		window.location = "gamer.html";
+	}
+	connexio.onerrors = function() { // Si la connexió té un error..
+		alert("Se ha interromput la connexió!");
+		window.location = "gamer.html";
+	}
+}
+// Missatge rebut pel servidor
+function receiveMessage() { /* Quan arriba un missatge, mostrar-lo per consola */
+	connexio.onmessage = function(message) {
+		let missatge = JSON.parse(message.data), nau;
+        nau = missatge.nau;
+        switch (missatge.msg) {
+            case "connected":      
+                createNau(nau); // Crear la nau
+                centerNau(); // Centrar la nau
+                //updateCanvas();
+                // Bucle
+                break;
+            case "moveSpaceShip":
+                spaceShip = nau;
+                spaceShip.img = new Image();
+                spaceShip.img.src = "../images/nau64px.png";
+                console.log(nau);
+                updateCanvas();
+                break;
+        }
+	}
 }
 
 function init() {
-
     var domini;
     if (window.location.protocol == "file:") domini = "localhost";
     else domini = window.location.hostname;
     var url = "ws://" + domini + ":8180";
     connexio = new WebSocket(url);
 
-    //Detecta quan s'apreten tecles i crida la funció moureNau.
-    connexio.onopen = (event) => {
+    // Obrir la conexió
+    openConnection();
+    // Message from server
+	receiveMessage();
 
-        connexio.send(JSON.stringify({ action: "addPlayer" }));
-
-        connexio.onmessage = (message) => {
-            var missatge = JSON.parse(message.data);
-            switch (missatge.msg) {
-                case "connected":
-                    console.log("Connecting user " + missatge.id);
-                    id = missatge.id;
-                    crearNau();
-                    updateCanvas(); 
-                    break;
-                case "printShips":
-                    console.log(missatge.coordenades);
-                    ubicarNaus(missatge.coordenades);
-                    break;
-            }
-        }
-
-    }
 }
 
 init();
